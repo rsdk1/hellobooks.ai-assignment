bab707886a7345c7064faf09cba7ded288c731c8
Suma D.K <78605884+rsdk1@users.noreply.github.com>
2025-12-26 11:43:56 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,11 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import fs from 'fs';
 
+// Ensure CTRF directory exists
+if (!fs.existsSync('ctrf')) {
+  fs.mkdirSync('ctrf');
+}
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -23,8 +28,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],['playwright-ctrf-json-reporter',{}]],
+  /*reporter: [['html'],['playwright-ctrf-json-reporter',{}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    ['list'],
+    ['json', { outputFile: 'ctrf/ctrf-report.json' }]
+  ],
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
@@ -78,4 +87,5 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
+
 
